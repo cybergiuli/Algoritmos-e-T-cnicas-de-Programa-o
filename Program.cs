6eef8729cb88a31f24af219ad60af97ba257525d
Ana Giulia Abreu <166351955+cybergiuli@users.noreply.github.com>
2024-06-21 09:21:35 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, var;
+            double[] temp = new double[31];
+           
+
+            for (i = 0; i < temp.Length; i++)
+            {
+                Console.WriteLine("escreva uma temperatura:");
+                temp[i] = double.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine("Escolha uma temperatura");
+            var = int.Parse(Console.ReadLine());
+
+            for (i = 0; i < temp.Length; i++)
+            {
+                if (var == temp[i])
+                {
+                    Console.WriteLine($"O dia é:{i + 1}");
+                }
+                
+            }
+
+            Console.Read();
+
+        }
+    }
+}
